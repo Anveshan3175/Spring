748e95bdc20f1cc47d6b85e359dbd40ac9aca5b3
Anveshan <akunduru4@gmail.com>
2017-08-21 02:43:56 +0530

    Signed-off-by: Anveshan <akunduru4@gmail.com>

--- emp/src/main/resources/Queries.sql
+++ emp/src/main/resources/Queries.sql
@@ -1,1 +1,7 @@
- create table tb_login(name varchar(20) not null,password varchar(20) not null,constraint login_pk primary key (name));
+--DDL
+create table tb_login(name varchar(20) not null,password varchar(20) not null,constraint login_pk primary key (name));
+ 
+ 
+ 
+--DML
+insert into tb_login values ('test','test');
