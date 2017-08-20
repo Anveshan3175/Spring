6a8db0a42b7ab793d5b05ffe5b8998fb14e6ad99
Anveshan <akunduru4@gmail.com>
2017-08-21 02:37:11 +0530

    Signed-off-by: Anveshan <akunduru4@gmail.com>

--- emp/src/main/java/com/emp/dao/LoginDAO.java
+++ emp/src/main/java/com/emp/dao/LoginDAO.java
@@ -1,6 +1,8 @@
 package com.emp.dao;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -9,6 +11,8 @@ import com.emp.model.Login;
 
 @Repository
 public class LoginDAO {
+	
+	private static final Logger logger = Logger.getLogger(LoginDAO.class);
 	
 	@Autowired
     JdbcTemplate jdbcTemplate;
@@ -19,9 +23,17 @@ public class LoginDAO {
 	}
 	
 	public boolean validateLogin(String name,String password) {
-		Login person = (Login) jdbcTemplate.queryForObject("SELECT * FROM tb_login where name = ? and password = ? ",
-            new Object[] { name, password}, new BeanPropertyRowMapper(Login.class));
- 
-        return (person != null);
+		Login person = null;
+		boolean isSuccess = false;
+		
+		try{
+			person = (Login) jdbcTemplate.queryForObject("SELECT * FROM tb_login where name = ? and password = ? ",
+		            new Object[] { name, password}, new BeanPropertyRowMapper(Login.class));
+			isSuccess = true;
+		}
+		catch(IncorrectResultSizeDataAccessException ex){
+			logger.error("No records found for user "+name);
+		}
+        return isSuccess;
     }
 }
