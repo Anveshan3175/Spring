2ba91dc037562665533ad026fe3518d172043587
Anveshan <akunduru4@gmail.com>
2019-02-25 23:28:39 +0530

    all src files

--- intv/src/main/java/com/anv/intv/config/WebAppContextConfig.java
+++ intv/src/main/java/com/anv/intv/config/WebAppContextConfig.java
@@ -1,5 +1,7 @@
 package com.anv.intv.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -10,33 +12,61 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages="com.anv.intv.controller")
+@ComponentScan(basePackages = "com.anv.intv.controller")
 public class WebAppContextConfig implements WebMvcConfigurer {
 
+    @Autowired
+    ApplicationContext applicationContext;
+    
 	@Override
-	   public void addViewControllers(ViewControllerRegistry registry) {
-	      registry.addViewController("/index");
-	   }
-	
-	   @Bean
-	   public ViewResolver viewResolver() {
-	      InternalResourceViewResolver bean = new InternalResourceViewResolver();
-	 
-	      bean.setViewClass(JstlView.class);
-	      bean.setPrefix("/WEB-INF/view/");
-	      bean.setSuffix(".jsp");
-	 
-	      return bean;
-	   }
-	   
-	    @Override
-	    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-	        registry
-	          .addResourceHandler("/resources/**")
-	          .addResourceLocations("/resources/")
-	          .setCachePeriod(31556926);
-	    }
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/index");
+	}
+
+	/*
+	 * @Bean public ViewResolver viewResolver() { InternalResourceViewResolver bean
+	 * = new InternalResourceViewResolver();
+	 * 
+	 * bean.setViewClass(JstlView.class); bean.setPrefix("/WEB-INF/view/jsp/");
+	 * bean.setSuffix(".jsp");
+	 * 
+	 * return bean; }
+	 */
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/").setCachePeriod(31556926);
+	}
+
+	// 1. Creating SpringResourceTemplateResolver
+	@Bean
+	public SpringResourceTemplateResolver springTemplateResolver() {
+		SpringResourceTemplateResolver springTemplateResolver = new SpringResourceTemplateResolver();
+		springTemplateResolver.setApplicationContext(this.applicationContext);
+		springTemplateResolver.setPrefix("/WEB-INF/view/thymeleaf/");
+		springTemplateResolver.setSuffix(".html");
+		return springTemplateResolver;
+	}
+
+	// 2. Creating SpringTemplateEngine
+	@Bean
+	public SpringTemplateEngine springTemplateEngine() {
+		SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
+		springTemplateEngine.setTemplateResolver(springTemplateResolver());
+		return springTemplateEngine;
+	}
+
+	// 3. Registering ThymeleafViewResolver
+	@Bean
+	public ViewResolver viewResolver() {
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(springTemplateEngine());
+		return viewResolver;
+	}
 }
--- intv/src/main/java/com/anv/intv/controller/LoginController.java
+++ intv/src/main/java/com/anv/intv/controller/LoginController.java
@@ -4,7 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.anv.intv.svc.IUserDetailsSVC;
 
@@ -20,6 +22,13 @@ public class LoginController {
     public String showForm() {
     	logger.debug("-------------- login invoked -----------------------");
         return "login";
+    }
+    
+    @PostMapping("/validateLogin")
+    public String validateLogin(Model model) {
+    	logger.debug("-------------- validateLogin invoked -----------------------");
+    	model.addAttribute("greeting", "Hi there , from controller");
+        return "home";
     }
     
     @GetMapping("/takeInterview")
