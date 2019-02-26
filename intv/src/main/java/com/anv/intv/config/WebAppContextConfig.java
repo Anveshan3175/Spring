15ef2c223466435c38772e379051752647d82e33
Anveshan <akunduru4@gmail.com>
2019-02-26 21:42:19 +0530

    all src files

--- intv/src/main/java/com/anv/intv/config/WebAppContextConfig.java
+++ intv/src/main/java/com/anv/intv/config/WebAppContextConfig.java
@@ -10,8 +10,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
--- intv/src/main/java/com/anv/intv/controller/LoginController.java
+++ intv/src/main/java/com/anv/intv/controller/LoginController.java
@@ -1,13 +1,17 @@
 package com.anv.intv.controller;
 
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.anv.intv.form.CandidateDetailsForm;
 import com.anv.intv.svc.IUserDetailsSVC;
 
 @Controller
@@ -15,32 +19,45 @@ public class LoginController {
 
 	@Autowired
 	private IUserDetailsSVC userSvc;
-	
+
 	Logger logger = LoggerFactory.getLogger(LoginController.class);
-	
-    @GetMapping("/login")
-    public String showForm() {
-    	logger.debug("-------------- login invoked -----------------------");
-        return "login";
-    }
-    
-    @PostMapping("/validateLogin")
-    public String validateLogin(Model model) {
-    	logger.debug("-------------- validateLogin invoked -----------------------");
-    	model.addAttribute("greeting", "Hi there , from controller");
-        return "home";
-    }
-    
-    @GetMapping("/takeInterview")
-    public String showInterviewForm() {
-    	logger.debug("-------------- Interview invoked -----------------------");
-    	boolean isValidlogin = userSvc.validateLoginUser("tester", "admin");
-        return "takeInterview";
-    }
-    
-    @GetMapping("/javaQuestions")
-    public String getJavaQuestions() {
-    	logger.debug("-------------- javaQuestions invoked -----------------------");
-        return "javaQuestions";
-    }
+
+	@GetMapping("/login")
+	public String showForm() {
+		logger.debug("-------------- login invoked -----------------------");
+		return "login";
+	}
+
+	@PostMapping("/validateLogin")
+	public String validateLogin(Model model) {
+		logger.debug("-------------- validateLogin invoked -----------------------");
+		model.addAttribute("greeting", "Hi there , from controller");
+		return "home";
+	}
+
+	@GetMapping("/takeInterview")
+	public String showInterviewForm() {
+		logger.debug("-------------- Interview invoked -----------------------");
+		boolean isValidlogin = userSvc.validateLoginUser("tester", "admin");
+		return "takeInterview";
+	}
+
+	@GetMapping("/javaQuestions")
+	public String getJavaQuestions() {
+		logger.debug("-------------- javaQuestions invoked -----------------------");
+		return "javaQuestions";
+	}
+
+	@GetMapping("/addcandidate")
+	public String showForm(CandidateDetailsForm candidateDetailsForm) {
+		return "candidateDetails";
+	}
+
+	@PostMapping("/validatecandidate")
+	public String checkPersonInfo(@Valid CandidateDetailsForm candidateDetailsForm, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "candidateDetails";
+		}
+		return "home"; 
+	}
 }
